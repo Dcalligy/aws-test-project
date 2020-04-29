556eb9ecb01e107f241c5f6d38a0d5f85f943406
MFUSF <60011641+MFUSF@users.noreply.github.com>
2020-04-29 18:24:25 -0400

    Add files via upload

---
+++ src/Groups.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+
+class Groups extends Component{
+    render(){
+        return (
+        <article>
+            <div className="container">
+                <h1>Groups</h1> 
+            </div>
+
+<div className="well wellSignUp">
+            <div className="container">
+                <div className="row">
+                <div className="col-lg-12"> 
+                <section className="section auth">
+            
+
+                <div className="panel-group">
+                    <h3 className="group-names">Group 1</h3>
+                    <div className="panel panel-default">
+                
+
+                <table className="table">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th scope="col">Last</th>
+                      <th scope="col">First</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Preferred Project(s)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Heartfilia</td>
+                      <td>Lucy</td>
+                      <td>LHeartfilia@mail.usf.edu</td>
+                      <td>2, 3, 4</td>
+                    </tr>
+                    <tr>
+                      <td>Dragneel</td>
+                      <td>Natsu</td>
+                      <td>NDragneel@mail.usf.edu</td>
+                      <td>1, 2, 4</td>
+                    </tr>
+                    <tr>
+                      <td>Scarlet</td>
+                      <td>Erza</td>
+                      <td>ESclarlet@mail.usf.edu</td>
+                      <td>2</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            
+            
+            <div className="panel-group">
+              <h3 className="group-names">Group 2</h3>
+              <div className="panel panel-default">
+
+
+                <table className="table">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th scope="col">Last</th>
+                      <th scope="col">First</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Preferred Project(s)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>McGarden</td>
+                      <td>Levy</td>
+                      <td>LMcGarden@mail.usf.edu</td>
+                      <td>1, 4</td>
+                    </tr>
+                    <tr>
+                      <td>Redfox</td>
+                      <td>Gajeel</td>
+                      <td>GRedfox@mail.usf.edu</td>
+                      <td>4, 5, 6</td>
+                    </tr>
+                    <tr>
+                      <td>Clive</td>
+                      <td>Gildarts</td>
+                      <td>GClive@mail.usf.edu</td>
+                      <td>2, 3, 4</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            
+            
+            <div className="panel-group">
+              <h3 className="group-names">Group 3</h3>
+              <div className="panel panel-default">
+
+                
+                <table className="table">
+                  <thead className="thead-dark">
+                    <tr>
+                      <th scope="col">Last</th>
+                      <th scope="col">First</th>
+                      <th scope="col">Email</th>
+                      <th scope="col">Preferred Project(s)</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Strauss</td>
+                      <td>Mirajane</td>
+                      <td>MStrauss2@mail.usf.edu</td>
+                      <td>1, 4</td>
+                    </tr>
+                    <tr>
+                      <td>Dreyar</td>
+                      <td>Laxus</td>
+                      <td>LDreyar@mail.usf.edu</td>
+                      <td>1</td>
+                    </tr>
+                    <tr>
+                      <td>Alberona</td>
+                      <td>Cana</td>
+                      <td>CAlberona@mail.usf.edu</td>
+                      <td>1, 2</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </section>
+
+      </div>
+    </div>
+    </div>
+      </div>
+    </article>
+    )
+    }
+}
+export default Groups;

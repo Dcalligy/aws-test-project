d7dbf96e373e0d8b4c0648c8d3ff0988be3bd71b
Dillon Calligy <dcalligy@mail.usf.edu>
2020-04-29 17:19:58 -0400

    Update Question.js

--- src/Question.js
+++ src/Question.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Checkbox from './Checkbox';
+//import Checkbox from './Checkbox';
 //import axios from "axios";
 //const config = require('config.json');
 
